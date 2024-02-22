8bdfc330d6e2524eac4fa9cb1d4b572a6a0bd575
Jason <jayvdb1@gmail.com>
2024-02-22 20:47:23 +0200

    Initial commit

---
+++ Pkarr/PkarrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PkarrApp.swift
+//  Pkarr
+//
+//  Created by Jason van den Berg on 2024/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PkarrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# pkarr-ios
+ 
